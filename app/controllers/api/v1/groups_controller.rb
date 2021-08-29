4694a266f1fb291adbc6f276757abcbf3f6a75bf
Kazuki Tsujimura <63531246+kazuki0122@users.noreply.github.com>
2021-08-29 15:45:57 +0900

    groupsコントローラー・usersコントローラーの作成

---
+++ app/controllers/api/v1/groups_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::GroupsController < ApplicationController
+  def index
+    
+    render json: {}
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :test, only: :index
+      resources :groups, only: :index
+      resources :users, only: :index
 
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
         registrations: 'api/v1/auth/registrations'
