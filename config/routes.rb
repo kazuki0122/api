2baf933da52455f3118e94a98b5ca9f4d9e7fe15
Kazuki Tsujimura <63531246+kazuki0122@users.noreply.github.com>
2021-09-02 13:38:10 +0900

    グループ機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :test, only: :index
-      resources :groups, only: :index
+      resources :groups, only: [:index, :create, :show]
       resources :users, only: :index
 
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
